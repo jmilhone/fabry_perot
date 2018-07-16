1fb84e7697c5eecffc52575e036f29a1fe503034
Jason Milhone <jmilhone@wisc.edu>
2018-07-16 13:25:14 -0500

    Added a decorator to handle registered types of images

--- fabry/tools/images.py
+++ fabry/tools/images.py
@@ -1,25 +1,62 @@
 import rawpy
 import exifread
 import numpy as np
-from os.path import isfile
+import os.path as path
+
+image_readers = []
+
+def register_reader(reader_func):
+    """
+    Decorator for registering functions as image readers
+    """
+    image_readers.append(reader_func)
 
 def check_nef(filename):
     if filename[-4:].lower() != '.nef':
-        if isfile(filename+'.NEF'):
+        if path.isfile(filename+'.NEF'):
             filename += '.NEF'
-        elif isfile(filename+'.nef'):
+        elif path.isfile(filename+'.nef'):
             filename += '.nef'
         else:
             raise Exception('{0} does not exist!'.format(filename))
     return filename
 
-def get_data(filename, color=None):
-    filename = check_nef(filename)
-    image = rawpy.imread(filename).postprocess(demosaic_algorithm=rawpy.DemosaicAlgorithm.LINEAR,
+@register_reader
+def read_nef(fname):
+    """
+    Reads .nef image files
+    """
+    if path.splitext(fname)[-1].lower() == '.nef':
+        image = rawpy.imread(fname)
+        data = image.postprocess(demosaic_algorithm=rawpy.DemosaicAlgorithm.LINEAR,
             output_color=rawpy.ColorSpace.raw, output_bps=16, no_auto_bright=True, 
             adjust_maximum_thr=0., gamma=(1, 1)).astype('float64')
 
-    if color is None:
+        return data 
+    else:
+        return None
+
+@register_reader
+def read_npy(fname):
+    """
+    Reads numpy binary files
+    """
+    if path.splitext(fname)[-1].lower() == ".npy":
+        data = np.load(fname)
+        return data
+    else:
+        return None
+
+def get_data(filename, color=None):
+
+    for reader in image_readers:
+        image = reader(filename)
+        if image is not None:
+            break
+    else:
+        raise ValueError('Image not found or not supported')
+
+    if color is None or len(image.shape) == 2:
         a = image
     elif color.lower() in ['r', 'red']:
         a = image[:,:,0]
