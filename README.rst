35478d541e6c4cf14c09ff0a878380c7252f750c
Jason Milhone <jmilhone@wisc.edu>
2018-11-13 12:14:53 -0600

    Adding packaging files

---
+++ README.rst
@@ -0,0 +1,10 @@
+**********
+Fabry
+**********
+
+Package for analyzing Fabry-Perot interference patterns
+
+Installation
+**************
+
+Rawpy and pymultinest require installing external libraries.
---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+        name="fabry",
+        version="0.1.0",
+        author="Jason Milhone",
+        author_email="jason.m.milhone@gmail.com",
+        description="Package for analyzing Fabry-Perot interference patterns",
+        long_description="",
+        url="https://git.doit.wisc.edu/KSFLANAGAN/python_FabryPerot",
+        packages=setuptools.find_packages(),
+        )
